091843ee076e28de451c25230f2186d4234a8001
Brian of London (Dot) <brian@v4v.app>
2023-05-14 17:28:15 +0300

    New Main layout
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayoutNew.vue'),
+    component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
