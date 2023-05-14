f32347de1c8d70bb23dddd47962d8debb17938c8
Brian of London (Dot) <brian@v4v.app>
2023-05-14 17:18:00 +0300

    Trying new layout style
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

---
+++ src/layouts/MainLayoutNew.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="lHh lpR lFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title> V4V.app v2 Dev </q-toolbar-title>
+        <LanguageSelector />
+        <DarkSelector />
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" elevated>
+      <div>Side Menu</div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title> Prices </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import LanguageSelector from "components/utils/LanguageSelector.vue";
+import DarkSelector from "components/utils/DarkSelector.vue";
+
+const rightDrawerOpen = ref(false);
+
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/MainLayoutNew.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
