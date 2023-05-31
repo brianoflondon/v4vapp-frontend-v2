b51ed6d19c4ce23af57f810ac0b3e45863df6b45
Brian of London (Dot) <brian@v4v.app>
2023-05-31 22:09:04 +0300

    Better responsive method
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- src/components/PriceBar.vue
+++ src/components/PriceBar.vue
@@ -1,72 +1,71 @@
 <template>
   <q-footer elevated>
     <div class="price-bar q-pa-none shadow-1 no-wrap row">
-      <div class="col-9 price-bar-item">
-        <span class="price-bar-item btc-price q-pa-xs">
-          <i class="fa-brands fa-btc" />&thinsp;
-          <strong>${{ storeAPIStatus.bitcoin }}</strong>
-        </span>
-        <span class="price-bar-item hive-price q-pa-xs">
-          <i class="fa-brands fa-hive" />&thinsp;
-          <strong>
-            ${{ storeAPIStatus.hive }}
-            &thinsp;/&thinsp;
-            {{ storeAPIStatus.hiveSats }} {{ $t("sats") }}
-          </strong>
-        </span>
-        <span ref="HBDItem" class="price-bar-item hbd-price q-pa-xs">
-          HBD
-          <strong>${{ storeAPIStatus.hbd }}</strong>
-        </span>
-      </div>
-      <div class="col-3 price-bar-item">
-        <span class="price-bar-item api-status-indicator q-pa-none">
-          <q-btn
-            @click="alert = true"
-            flat
-            dense
-            :title="storeAPIStatus.apiError ? $t('failure') : $t('working')"
-          >
-            {{ storeAPIStatus.statusDisp }}
-          </q-btn>
-        </span>
-        <span class="price-bar-item reload-status q-pa-none">
-          <q-btn
-            icon="refresh"
-            :title="$t('reload_prices')"
-            flat
-            dense
-            @click="storeAPIStatus.update()"
-          />
-        </span>
-        <span class="price-bar-item keychain-status-indicator">
-          <q-btn
-            flat
-            dense
-            :title="
+      <span class="price-bar-item btc-price q-pa-xs">
+        <i class="fa-brands fa-btc" />&thinsp;
+        <strong>${{ storeAPIStatus.bitcoin }}</strong>
+      </span>
+      <spacer />
+      <span class="price-bar-item hive-price q-pa-xs">
+        <i class="fa-brands fa-hive" />&thinsp;
+        <strong>
+          ${{ storeAPIStatus.hive }}
+          &thinsp;/&thinsp;
+          {{ storeAPIStatus.hiveSats }}{{ $t("sats") }}
+        </strong>
+      </span>
+      <span v-if="!smallScreen" class="price-bar-item hbd-price q-pa-xs">
+        <span><i class="fa-brands fa-hive" /></span>
+        <strong>${{ storeAPIStatus.hbd }}</strong>
+      </span>
+      <span class="price-bar-item api-status-indicator q-pl-lg q-pa-xs">
+        <q-btn
+          @click="alert = true"
+          flat
+          dense
+          :title="storeAPIStatus.apiError ? $t('failure') : $t('working')"
+        >
+          {{ storeAPIStatus.statusDisp }}
+        </q-btn>
+      </span>
+      <span class="price-bar-item reload-status q-pa-xs">
+        <q-btn
+          icon="refresh"
+          :title="$t('reload_prices')"
+          flat
+          dense
+          @click="storeAPIStatus.update()"
+        />
+      </span>
+      <span class="price-bar-item keychain-status-indicator q-pa-xs">
+        <q-btn
+          flat
+          dense
+          :title="
+            storeAPIStatus.isKeychainIn
+              ? $t('keychain_installed')
+              : $t('keychain_missing')
+          "
+        >
+          <img
+            :src="
               storeAPIStatus.isKeychainIn
-                ? $t('keychain_installed')
-                : $t('keychain_missing')
+                ? '/keychain/hive-keychain-keys.svg'
+                : '/keychain/hive-keychain-keys-missing.svg'
             "
-          >
-            <img
-              :src="
-                storeAPIStatus.isKeychainIn
-                  ? '/keychain/hive-keychain-keys.svg'
-                  : '/keychain/hive-keychain-keys-missing.svg'
-              "
-              style="height: 15px; max-width: 15px"
-            />
-          </q-btn>
-        </span>
+            style="height: 15px; max-width: 15px"
+          />
+        </q-btn>
+      </span>
+      <span class="price-bar-item dark-selector q-pa-xs">
         <DarkSelector />
-      </div>
+      </span>
     </div>
   </q-footer>
 </template>
 
 <script setup>
-import { onMounted, onUnmounted, ref, watch } from "vue"
+import { onMounted, onUnmounted, computed } from "vue"
 import DarkSelector from "components/utils/DarkSelector.vue"
 import { useStoreAPIStatus } from "src/stores/storeAPIStatus"
 import { useI18n } from "vue-i18n"
@@ -76,21 +75,11 @@ const storeAPIStatus = useStoreAPIStatus()
 const t = useI18n().t
 const q = useQuasar()
 
-const HBDItem = ref(null)
-
 let timeoutId
 
-// watch the q.screen for changes
-watch(
-  () => q.screen.width,
-  (val) => {
-    if (val < 425) {
-      console.log((HBDItem.value.style.display = "none"))
-    } else {
-      console.log((HBDItem.value.style.display = "block"))
-    }
-  }
-)
+const smallScreen = computed(() => {
+  return q.screen.width < 460
+})
 
 // run on mounted
 onMounted(() => {
@@ -110,7 +99,7 @@ onUnmounted(() => {
 </script>
 
 <style lang="scss" scoped>
-.price-bar-item {
+.price-bar {
   display: flex;
   justify-content: center;
   align-items: center;
