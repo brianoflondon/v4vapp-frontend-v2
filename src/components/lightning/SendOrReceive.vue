42d55bf4b49115906ad2190395785221d0ffe60a
Brian of London (Dot) <brian@v4v.app>
2023-06-24 18:54:54 +0300

    send receive tabs
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- src/components/lightning/SendOrReceive.vue
+++ src/components/lightning/SendOrReceive.vue
@@ -2,16 +2,16 @@
   <div class="send-receive-selector">
     <div class="q-gutter-y-md" style="max-width: 550px">
       <q-tabs dense>
-        <q-route-tab name="send" to="/" label="Send" />
-        <q-route-tab name="receive" to="/status" label="Receive" />
+        <q-route-tab name="send" to="/" :label="$t('send')" />
+        <q-route-tab name="receive" to="/status" :label="$t('receive')" />
       </q-tabs>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
-const tab = ref("send")
+import { useI18n } from "vue-i18n"
+const t = useI18n().t
 </script>
 
 <style lang="scss" scoped></style>
