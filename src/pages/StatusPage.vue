db90e771503755680522f6c8fc226f6136c6c82a
Brian of London (Dot) <brian@v4v.app>
2023-06-27 16:15:24 +0300

    Version number on status page npm update
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- src/pages/StatusPage.vue
+++ src/pages/StatusPage.vue
@@ -12,21 +12,22 @@
           <q-card-section>
             <div class="text-overline text-orange-9">V4V.app</div>
             <div class="text-h5 q-mt-sm q-mb-xs">{{ $t("status") }}</div>
-            <a
-              href="https://peakd.com/hive-110369/@v4vapp/hive-to-lightning-gateway-fees"
-              target="_blank"
-            >
-              <div class="text-caption text-grey">
+            <div class="text-body1">
+              <a
+                href="https://peakd.com/hive-110369/@v4vapp/hive-to-lightning-gateway-fees"
+                target="_blank"
+              >
                 {{ $t("status_page_message") }}
-              </div>
-            </a>
+              </a>
+              <p class="text-right text-caption">{{ appName }} v{{ appVersion }}</p>
+            </div>
           </q-card-section>
           <q-card-section>
             <div class="vote-button q-pa-lg text-center">
               <VoteProposal v-model="voteOptions" />
             </div>
           </q-card-section>
-      </q-card>
+        </q-card>
       </div>
     </div>
   </q-page>
@@ -34,9 +35,11 @@
 
 <script setup>
 import { ref } from "vue"
-import { useI18n } from "vue-i18n"
 import VoteProposal from "components/utils/VoteProposal.vue"
-const { t } = useI18n()
+import { useAppDetails } from "src/use/useAppDetails.js"
+
+const { appName, appVersion } = useAppDetails()
+console.log("appName", appName, "appVersion", appVersion)
 
 const voteOptions = ref({
   hiveUser: "",
