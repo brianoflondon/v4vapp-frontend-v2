4ecc85eba2e0834c361e61236c4b14c2d5766b08
Brian of London (Dot) <brian@v4v.app>
2023-06-28 12:09:37 +0300

    Adds HBD buttons to the details page
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

---
+++ src/components/utils/NumberButtons.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="fit row wrap justify-center items-end content-stretch q-gutter-sm"
+  >
+    <div v-for="n in buttonValues" :key="n">
+      <q-btn
+        dense
+        rounded
+        size="md"
+        color="green-10"
+        :label="`$${n}`"
+        @click="emit('buttonPressed', n)"
+        style="min-width: 75px"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+const buttonValues = ref([1, 5, 10, 20, 50, 75, 100])
+const emit = defineEmits(["buttonPressed"])
+</script>
+
+<style lang="scss" scoped></style>
