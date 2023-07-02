2a4d815e1d5f99158f92c1e616e0ad8d0026948b
Brian of London <bol@snnsite.com>
2023-07-02 11:11:29 +0300

    New translations index.js (Spanish)

--- src/i18n/es-ES/index.js
+++ src/i18n/es-ES/index.js
@@ -1,10 +1,107 @@
 // This is just an example,
 // so you can safely delete all default props below
-
 export default {
-  failed: "Action failed",
-  success: "Action was successful",
-
+  // General
+  loading: 'Cargando...',
+  error: 'Error',
+  paste: 'Pegar',
+  required: 'Requerido',
+  ok: 'Ok',
+  cancel: 'Cancelar',
+  characters: 'caracteres',
+  hours: 'horas',
+  minutes: 'minutos',
+  seconds: 'segundos',
+  fee: 'comisión',
+  vote: 'Votaciones',
+  please: 'Por favor',
+  and: 'y',
+  witness: 'Mi Hive Testigo',
+  // Keychain
+  keychain_installed: 'Keychain está instalado',
+  keychain_missing: 'Keychain is Missing',
   // IndexPage
-  index_page_message: "Para obtener más información sobre este proyecto, visite",
+  index_page_message: 'Para obtener más información sobre este proyecto, visite',
+  // Selector
+  hive_account: 'Hive Account',
+  // Lightning Page
+  lightning: 'Lightning',
+  hive: 'Hive',
+  // Login
+  keychain_not_installed: 'Keychain is not installed',
+  enter_hive_account: 'Please enter you Hive Account name',
+  hive_login: 'Hive Login',
+  login: 'Login',
+  login_as: 'Login as',
+  login_with: 'Login with',
+  hive_keychain: 'Hive Keychain',
+  login_in_progress: 'Login in progress',
+  login_failed: 'Login failed',
+  login_success: 'Login success',
+  sign_this: 'Sign this',
+  matches: 'matches',
+  // Get Hive
+  amount: 'Cantidad',
+  copy: 'Copiar',
+  copy_qrcode: 'Haga clic para copiar el código QR al portapapeles',
+  pay: 'Pagar',
+  pay_tooltip: 'Paga esta factura con la aplicación Lighting instalada',
+  copied: 'Copiado al portapapeles',
+  download: 'Download',
+  download_tooltip: 'Download QR code as a file',
+  downloading_qrcode: 'Downloading QR code',
+  // SideMenu
+  page: 'Page',
+  home: 'Home',
+  status: 'Status',
+  // Status Page
+  status_page_message: 'Status page is under construction, in the meantime please visit Hive.',
+  // Pricebar
+  sats: 'シ',
+  failure: 'Failure',
+  working: 'Working',
+  keychain_installed: 'Keychain is Installed',
+  keychain_missing: 'Keychain is Missing',
+  reload_prices: 'Reload Prices',
+  // Invoice
+  invoice: 'Invoice',
+  expires: 'expires',
+  enter_invoice: 'lnbc.... o Lightning Address',
+  valid_invoice: 'Invoice decoded',
+  valid_lightning_address: 'Address decoded',
+  invalid_invoice: 'Invalid invoice or Lightning Address',
+  invoice_too_low: 'Invoice is below minimum payment',
+  invoice_too_high: 'Invoice is above maximum payment',
+  invoice_expired: 'Invoice has expired',
+  self_payment: 'Can\'t send to v4v.app',
+  invoice_hint: 'Paste an invoice or scan a QR code',
+  waiting_for: 'Waiting for reply transaction to appear on Hive blockchain',
+  // Please vote
+  voting_as: 'Voting as',
+  vote_for_proposal: 'Vote DHF for proposal',
+  please_vote: 'If you\'re using V4V.app and find it useful, please consider voting for both my proposal for ongoing maintenance and my witness. This really helps me keep the service operating. Thank you!',
+  thank_you_for_voting: 'Thank you for voting!',
+  vote_witness: 'Vote for my Hive Witness',
+  vote_proposal: 'Vote for my DHF Proposal',
+  thank_you: 'Thank you for voting already!',
+  // Tab Bar
+  send: 'Send',
+  receive: 'Receive',
+  podcasts: 'Podcasts',
+  pods: 'Pods',
+  // Ask Details dialog
+  asking_details: 'You are about to pay:',
+  making_invoice: 'You are about to create an invoice for:',
+  amount_to_send: 'Amount to send',
+  too_low: 'Amount too low',
+  too_high: 'Amount too high',
+  comment_length: 'Comment is too long',
+  // Camera
+  NotAllowedError: 'You need to grant camera access permission',
+  NotSupportedError: 'No camera found on this device',
+  NotReadableError: 'Is the camera already in use?',
+  OverconstrainedError: 'Installed cameras are not suitable',
+  StreamApiNotSupportedError: 'Stream API is not supported in this browser',
+  InsecureContextError: 'Secure secure context required (HTTPS, localhost)',
+  OtherError: 'Unknown camera error'
 };
