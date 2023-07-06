3f6d0dabe80aecf0cc9a7f064bf9dfa198ec5e2f
Brian of London (Dot) <brian@v4v.app>
2023-07-06 12:11:30 +0300

    Gives time of update
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- src/components/PriceBar.vue
+++ src/components/PriceBar.vue
@@ -39,6 +39,7 @@
           dense
           @click="storeAPIStatus.update()"
         />
+        <q-tooltip>{{ lastUpdate }}</q-tooltip>
       </span>
       <span class="price-bar-item keychain-status-indicator q-pa-xs">
         <q-btn
@@ -68,16 +69,18 @@
 </template>
 
 <script setup>
-import { onMounted, onUnmounted, computed } from "vue"
+import { onMounted, onUnmounted, computed, ref } from "vue"
 import DarkSelector from "components/utils/DarkSelector.vue"
 import { useStoreAPIStatus } from "src/stores/storeAPIStatus"
+import { useDateFormat } from "@vueuse/core"
 import { useI18n } from "vue-i18n"
 import { useQuasar } from "quasar"
 
 const storeAPIStatus = useStoreAPIStatus()
 const t = useI18n().t
 const q = useQuasar()
-
+const lastUpdate = ref("")
+const lastFetch = useDateFormat(storeAPIStatus.fetchTimestamp, "HH:mm:ss")
 let timeoutId
 
 const smallScreen = computed(() => {
@@ -90,10 +93,11 @@ onMounted(() => {
 })
 
 async function scheduleUpdate() {
-  console.log("Updating prices")
+  // console.log("Updating prices")
   await storeAPIStatus.update()
   // Schedule the next update after 5 minutes
   timeoutId = setTimeout(scheduleUpdate, 10 * 60 * 1000)
+  lastUpdate.value = `${t("prices_fetched")}: ${lastFetch.value}`
 }
 
 onUnmounted(() => {
