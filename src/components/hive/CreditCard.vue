8db6207ed18ebf4d0eab8b53e46b748e410ea8f3
Brian of London (Dot) <brian@v4v.app>
2023-08-30 10:14:11 +0300

    remove a console log
    Signed-off-by: Brian of London (Dot) <brian@v4v.app>

--- src/components/hive/CreditCard.vue
+++ src/components/hive/CreditCard.vue
@@ -66,8 +66,10 @@
             >
               <strong>{{ balances["sats"] }}</strong
               ><br />
-              <div style="font-size: 0.7rem; line-height: 0.3rem;">
-                +<q-icon name="savings"></q-icon>&nbsp;{{ balances["totalSats"] }}
+              <div style="font-size: 0.7rem; line-height: 0.3rem">
+                +<q-icon name="savings"></q-icon>&nbsp;{{
+                  balances["totalSats"]
+                }}
               </div>
             </td>
             <td>
@@ -113,11 +115,6 @@ const lightDark = computed(() => {
 })
 
 const balances = computed(() => {
-  console.log(
-    storeUser.satsBalance,
-    storeUser.savingsSatsBalance,
-    storeUser.totalSatsBalance
-  )
   if (savingsToggle.value) {
     return {
       hive: storeUser.savingsHiveBalance,
