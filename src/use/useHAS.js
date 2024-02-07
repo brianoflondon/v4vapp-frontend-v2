857ddc30b44fd048dc9378d988bce06fb6eb3660
Brian of London (home imac) <brian@v4v.app>
2024-02-07 18:01:11 +0200

    Remove trailing /
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/use/useHAS.js
+++ src/use/useHAS.js
@@ -137,7 +137,7 @@ async function resolveAuth(res, auth, challenge_data) {
     console.log("formData", formData)
 
     console.log("apiLogin", apiLogin)
-    const responseApi = await apiLogin.post(`/token/`, formData)
+    const responseApi = await apiLogin.post(`/token`, formData)
     console.log("responseApi", responseApi)
     storeUser.login(
       auth_payload.account,
