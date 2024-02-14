83c5acf8b20a6d5d24150abf4bdeb165ebed40b6
Brian of London (home imac) <brian@v4v.app>
2024-02-14 12:40:02 +0200

    Remove console.log statements from useKeepSats function
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/use/useV4vapp.js
+++ src/use/useV4vapp.js
@@ -25,12 +25,10 @@ export async function useCheckApiTokenValid(username, apiToken) {
 }
 
 export async function useKeepSats(username, apiToken, hasApiToken) {
-  console.log("useKeepSats", username, apiToken)
   if (!apiToken) return null
   apiLogin.defaults.headers.common["Authorization"] = `Bearer ${apiToken}`
   try {
     const resp = await apiLogin.get("/v1/v4vapp/keepsats")
-    console.log("useKeepSats", resp.status, resp.data)
     return resp.data
   } catch (error) {
     console.log("useKeepSats", error)
