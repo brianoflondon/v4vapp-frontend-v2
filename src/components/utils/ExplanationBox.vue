4ed4947907649d9c6f174f1cd77ca1db4c246b7b
Brian of London (home imac) <brian@v4v.app>
2024-02-22 09:47:39 +0200

    Refactor ExplanationBox component to use props for title and text
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/components/utils/ExplanationBox.vue
+++ src/components/utils/ExplanationBox.vue
@@ -1,21 +1,55 @@
 <template>
   <!-- default-opened -->
-  <q-expansion-item
-    icon="question_answer"
-    :label="$t('explanation_title')"
-  >
+  <q-expansion-item icon="question_answer" :label="titleHere" dense>
     <q-card-section
       class="multiline-container text-left q-pa-xs"
       style="white-space: pre-line"
     >
       <div class="multiline-text">
-        {{ $t("explanation_text") }}
+        {{ textHere }}
       </div>
     </q-card-section>
   </q-expansion-item>
 </template>
 
-<script setup></script>
+<script setup>
+// add props for the title and text
+import { defineProps, onMounted, ref } from "vue"
+import { useI18n } from "vue-i18n"
+const t = useI18n().t
+
+const titleHere = ref("")
+const textHere = ref("")
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+    default: "",
+  },
+  text: {
+    type: String,
+    required: true,
+    default: "",
+  },
+})
+
+onMounted(() => {
+  console.log("ExplanationBox mounted")
+  console.log(props.title)
+  console.log(props.text)
+  if (props.title === "") {
+    titleHere.value = t("explanation_title")
+  } else {
+    titleHere.value = props.title
+  }
+  if (props.text === "") {
+    textHere.value = t("explanation_text")
+  } else {
+    textHere.value = props.text
+  }
+})
+</script>
 
 <style lang="scss" scoped>
 .multiline-container {
