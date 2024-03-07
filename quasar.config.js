0989d7a2d03dea942c9f87ca1509d578b4db8196
Brian of London (home imac) <brian@v4v.app>
2024-03-07 20:58:07 +0200

    Update capacitor hideSplashscreen option
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- quasar.config.js
+++ quasar.config.js
@@ -194,7 +194,8 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
     capacitor: {
-      hideSplashscreen: true,
+      hideSplashscreen: false,
+      iosStatusBarPadding: true,
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
