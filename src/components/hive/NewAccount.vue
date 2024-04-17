314f428dc6cc2c720c4d825fc01efa5eedf8b420
Brian of London (home imac) <brian@v4v.app>
2024-04-17 10:04:32 +0300

    Update NewAccount.vue component to improve UI and add form submission functionality.
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/components/hive/NewAccount.vue
+++ src/components/hive/NewAccount.vue
@@ -1,83 +1,118 @@
 <template>
-  <div class="align-center" style="max-width: 300px">
-    <q-input
-      v-model="accountName"
-      label="Account Name"
-      outlined
-      dense
-      debounce="500"
-      @update:model-value="checkHiveAccountName"
-      :error="!nameCheck"
-      :error-message="nameCheckError"
-    >
-      <template v-slot:prepend>
-        <q-icon name="person" />
-      </template>
-      <template v-slot:append>
-        <q-icon name="check" v-if="nameCheck" />
-      </template>
-    </q-input>
-    <q-slider
-      v-model="accountName.length"
-      :step="1"
-      :min="0"
-      :inner-min="3"
-      :inner-max="16"
-      :max="20"
-      :color="nameCheck ? 'primary' : 'negative'"
-      dense
-      readonly
-
-    >
-    </q-slider>
-    <q-input
-      class="q-mt-md"
-      v-model="masterPassword"
-      label="Master Password"
-      outlined
-      dense
-      debounce="500"
-      @update:model-value="generateKeys"
-    ></q-input>
-    <q-btn
-      label="Random Master Password"
-      color="primary"
-      @click="randomMasterPassword"
-    ></q-btn>
-    <q-btn label="Generate Keys" color="primary" @click="generateKeys"></q-btn>
-    <q-btn label="Download Keys" color="primary" @click="downloadKeys"></q-btn>
-    <pre>
-        {{ keys }}
-    </pre>
+  <div class="flex column text-center items-center q-pa-none">
+    <q-form @submit="handleSubmit" @reset="handleReset">
+      <q-input
+        class="large-font"
+        v-model="accountName"
+        label="Hive Account Name"
+        outlined
+        debounce="500"
+        clearable
+        @update:model-value="checkHiveAccountName"
+        :error="!nameCheck"
+        :error-message="nameCheckError"
+      >
+        <template v-slot:prepend>
+          <q-icon name="person" />
+        </template>
+        <template v-slot:append>
+          <q-icon name="check" v-if="nameCheck" />
+        </template>
+      </q-input>
+      <q-slider
+        v-model="accountName.length"
+        :label-value="accountName.length"
+        :step="1"
+        :min="0"
+        :inner-min="3"
+        :inner-max="16"
+        :max="20"
+        :color="nameCheck ? 'primary' : 'negative'"
+        dense
+        readonly
+      >
+      </q-slider>
+      <q-input
+        class="q-my-md"
+        v-model="masterPassword"
+        label="Master Password"
+        outlined
+        dense
+        debounce="500"
+        @update:model-value="generateKeys"
+      ></q-input>
+      <q-btn
+        class="q-ma-sm"
+        label="Random Master Password"
+        color="primary"
+        @click="randomMasterPassword"
+      ></q-btn>
+      <q-btn
+        class="q-ma-sm"
+        label="Generate Keys"
+        color="primary"
+        @click="generateKeys"
+      ></q-btn>
+      <q-btn
+        class="q-ma-sm"
+        label="Download Keys"
+        :disable="!nameCheck"
+        color="primary"
+        @click="downloadKeys"
+      ></q-btn>
+      <input type="submit" value="Sign In" />
+      <!-- <div v-if="nameCheck">
+        <input name="ownerKey" type="password" :value="keys?.private.owner" />
+        <input name="activeKey" type="password" :value="keys?.private.active" />
+        <input name="postingKey" type="password" :value="keys?.private.posting" />
+        <input name="memoKey" type="password" :value="keys?.private.memo" />
+        <input name="ownerPubKey" :value="keys?.public.owner" />
+        <input name="activePubKey" :value="keys?.public.active" />
+        <input name="postingPubKey" :value="keys?.public.posting" />
+        <input name="memoPubKey" :value="keys?.public.memo" />
+      </div> -->
+    </q-form>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue"
+import { ref } from "vue"
 import { useHiveAccountExists } from "src/use/useHive"
 import { PrivateKey } from "@hiveio/dhive"
 
 // Define the account name and master password
 const accountName = ref("")
-const nameCheck = ref(false)
+const nameCheck = ref(true)
 const nameCheckError = ref("")
 const masterPassword = ref("")
 const keys = ref({})
 
+function handleSubmit() {
+  console.log("submit pressed")
+}
+
+function handleReset() {
+  console.log("reset pressed")
+}
+
 async function checkHiveAccountName() {
+  if (accountName.value === null || accountName.value === "") {
+    accountName.value = ""
+    nameCheck.value = true
+    nameCheckError.value = ""
+    return
+  }
   accountName.value = accountName.value.toLowerCase()
   const resp = await useHiveAccountExists(accountName.value)
-  console.log("checkHiveAccountName", resp)
   nameCheck.value = resp.valid
   nameCheckError.value = resp.error
   if (resp.exists) {
-    console.log("Account exists")
     keys.value = {}
   } else if (resp.valid) {
     randomMasterPassword()
     generateKeys()
   } else {
-    console.log("resp.error", resp.error)
+    console.error("resp.error", resp.error)
   }
 }
 
@@ -165,4 +200,8 @@ Backup (Master) Password: ${masterPassword.value}
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.large-font {
+  font-size: 1.8rem;
+}
+</style>
