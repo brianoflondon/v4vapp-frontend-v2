806ef51a5227b419c9e6df8556a8d2461278df19
Brian of London (home imac) <brian@v4v.app>
2024-06-04 10:38:55 +0300

    feat: Add currency conversion functionality to ConvertWrapper.vue
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/components/hive/ConvertWrapper.vue
+++ src/components/hive/ConvertWrapper.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="q-pa-sm col justify-evenly">
+    <div class="flex justify-center">
+      <q-input
+        outlined
+        v-model="fromAmount"
+        label="From Amount"
+        type="number"
+      />
+      <q-select
+        outlined
+        v-model="fromCurrency"
+        label="From Currency"
+        :options="[
+          { label: 'HBD', value: 'hbd' },
+          { label: 'HIVE', value: 'hive' },
+          { label: 'SATS', value: 'sats' },
+        ]"
+      />
+    </div>
+    <div class="flex row justify-center">
+      <q-icon name="currency_exchange" size="2rem" />
+    </div>
+
+    <div class="flex justify-center">
+      <q-input outlined v-model="toAmount" label="To Amount" type="number" />
+      <q-select
+        outlined
+        v-model="toCurrency"
+        label="to Currency"
+        :options="[
+          { label: 'HBD', value: 'hbd' },
+          { label: 'HIVE', value: 'hive' },
+          { label: 'SATS', value: 'sats' },
+        ]"
+      />
+    </div>
+
     <div class="q-pa-sm">
       <q-tabs v-model="convertTab">
         <q-tab name="toHive" label="Sats to Hive" class="text-center"></q-tab>
@@ -24,10 +60,14 @@ import ConvertKeepsats from "src/components/hive/ConvertKeepsats.vue"
 import ReceiveKeepsats from "src/components/hive/ReceiveKeepsats.vue"
 
 const convertTab = ref("toHive")
+const fromCurrency = ref("SATS")
+const toCurrency = ref("HBD")
+const fromAmount = ref(0)
+const toAmount = ref(0)
 </script>
 
 <style lang="scss" scoped>
 .bordered-div {
-    border: 1px solid #ccc;
+  border: 1px solid #ccc;
 }
 </style>
