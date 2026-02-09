37dd8b837aaa986b9f232c20f9ac03c8e37e34f8
Brian of London (home imac) <brian@v4v.app>
2026-02-09 18:38:22 +0200

    Add maintenance notice popup with Latest News button

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,37 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
+import { Dialog } from "quasar";
 
 export default defineComponent({
   name: "App",
+  setup() {
+    // Toggle this to true/false to show/hide the message of the day
+    const showMOTD = true;
+
+    onMounted(() => {
+      if (showMOTD) {
+        const dialog = Dialog.create({
+          title: "Maintenance Notice",
+          message:
+            "v4v.app will be down for a big upgrade starting on Wednesday 11th February at 10am UTC. I hope to have it back up and running within a few hours but this may take up to two days.",
+          persistent: true,
+          ok: "Latest News",
+          cancel: "OK",
+        });
+
+        dialog.onOk(() => {
+          window.open("https://peakd.com/@v4vapp", "_blank");
+        });
+
+        dialog.onCancel(() => {
+          // Just close
+        });
+      }
+    });
+
+    return {};
+  },
 });
 </script>
