13576daf1433fc3af9bd384a06c0b1eec09ad047
Brian of London (home imac) <brian@v4v.app>
2026-02-16 20:40:07 +0200

    fix: update maintenance notice message for re-launch announcement
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,40 @@
 </template>
 
 <script>
-import { defineComponent, onMounted } from "vue";
-import { Dialog } from "quasar";
+import { defineComponent, onMounted } from "vue"
+import { Dialog } from "quasar"
 
 export default defineComponent({
   name: "App",
   setup() {
     // Toggle this to true/false to show/hide the message of the day
-    const showMOTD = true;
+    const showMOTD = true
 
     onMounted(() => {
       if (showMOTD) {
         const dialog = Dialog.create({
           title: "Maintenance Notice",
           message:
-            "v4v.app is still down but getting close to re-launch. PLEASE DO NOT USE THIS SITE UNTIL FURTHER NOTICE. We will post updates on our Hive blog and X",
+            "v4v.app is now tentatively back online, you can try it if you're brave, announcement coming in a few hours. We will post updates on our Hive blog and X",
           persistent: true,
           ok: "Latest News",
           cancel: "OK",
-        });
+        })
 
         dialog.onOk(() => {
-          window.open("https://peakd.com/hive-110369/@v4vapp/v4vapp-will-be-down-wednesday-11th-february-10am-utc", "_blank");
-        });
+          window.open(
+            "https://peakd.com/hive-110369/@v4vapp/v4vapp-will-be-down-wednesday-11th-february-10am-utc",
+            "_blank",
+          )
+        })
 
         dialog.onCancel(() => {
           // Just close
-        });
+        })
       }
-    });
+    })
 
-    return {};
+    return {}
   },
-});
+})
 </script>
